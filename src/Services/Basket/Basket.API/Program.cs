cfe1228304a70c830c751e00ff23f6ba9c98093d
Victor Zuñiga <VirozU@users.noreply.github.com>
2022-12-22 11:46:48 -0600

    Add Connection to Redis cache DB

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,11 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( c =>
+{
+    c.SwaggerDoc("v1",new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Basket.API",Version="v1"});
+});
 
 var app = builder.Build();
 
