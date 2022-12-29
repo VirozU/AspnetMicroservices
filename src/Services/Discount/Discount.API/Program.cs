c70f25bed0775505fb8ccc71c1b24d718f7c4b99
Victor Zuñiga <VirozU@users.noreply.github.com>
2022-12-28 19:09:58 -0600

    Contaniner for Discount.API

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,7 +1,10 @@
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MigrateDatabase<Program>();
 
 app.UseHttpsRedirection();
 
